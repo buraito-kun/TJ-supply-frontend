5444eba1785890f4d544b06537c84c288b4c50d0
Akaru1xR1N <brightrock19220@outlook.co.th>
2024-10-05 21:04:14 +0700

    Fix build failed

---
+++ app/qc/testSetting/create/qcType/page.js
@@ -0,0 +1,20 @@
+"use client"
+
+export default function QcType(){
+  return (
+    <>
+      <div className="flex w-screen h-screen">
+        <SideBar className="flex w-auto h-auto" />
+        <div className="flex flex-col w-full h-full">
+          <Breadcrumb className="w-auto h-auto" />
+          <div className="flex flex-col bg-white w-full h-full overflow-y-auto whitespace-nowrap place-items-center pt-[20px]">
+            
+            <div className="flex flex-col-reverse place-content-center">
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
